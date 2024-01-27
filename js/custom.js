9ad9d00d5aa2c71c37d6c5b22df1b0e5cfc9b9c7
freeman <iviewn@gmail.com>
2024-01-28 01:06:11 +0300

    index

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,21 @@
-
+/*mobile header btns*/
+$('.header__menu-btn').click(function () {
+    $('.header__nav-wrap').toggleClass('vis');
+    $('body').toggleClass('oh open-menu');
+    $('.hamburger').toggleClass('open');
+    $('.header__tel').removeClass('vis');
+});
+$('.header__nav > .parent > button').click(function () {
+    $(this).parent().hasClass('vis') ? $(this).parent().removeClass('vis') : $(this).parent().addClass('vis');
+    $(this).parent().find('.parent').removeClass('vis');
+});
+$('.header__nav .parent ul li.parent button').click(function () {
+    $(this).parent().hasClass('vis') ? $(this).parent().removeClass('vis') : $(this).parent().addClass('vis');
+});
+$('.header__tel-btn').click(function () {
+    $('.header__tel').toggleClass('vis');
+});
+/*end mobile header btns*/
 
 /*animation on scroll*/
 if($('.wow').length){
